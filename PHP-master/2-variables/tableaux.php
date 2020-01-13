f10e6bb21c335dc46b8d5cf7120a8e25c3a5423d
sidi-64ers <sidichahid@gmail.com>
2020-01-13 12:13:12 +0100

    fonction tableau php

--- PHP-master/2-variables/tableaux.php
+++ PHP-master/2-variables/tableaux.php
@@ -8,12 +8,25 @@ var_dump($tabCrochets);
 
 $jours = array("Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi");
 
-var_dump($jours);
+var_dump($jours); //display ($jours)
 
 echo $jours[2];
 
 array_push ($jours, "dimanche"); // add sunday
 
 var_dump($jours);
+
+array_pop($jours); //delete the last element of the tableau 
+
+var_dump($jours);
+
+unset($jours[3]); //destroy a variable index 3
+var_dump($jours);
+
+
+array_shift($jours); // delete the first element of the tableau
+var_dump($jours);
+
+
 
 ?>
