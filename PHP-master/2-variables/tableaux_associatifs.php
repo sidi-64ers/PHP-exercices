2ded30a058146c821145d0338f7632e914947f95
sidi-64ers <sidichahid@gmail.com>
2020-01-14 11:50:40 +0100

    tableaux

--- PHP-master/2-variables/tableaux_associatifs.php
+++ PHP-master/2-variables/tableaux_associatifs.php
@@ -1,14 +1,26 @@
 <?php
 
 $activites = array("Lundi"=>"Php","Mardi"=>"Javascript", "Mercredi"=>"Typescript","Jeudi"=>"Angular","Vendredi"=>"SQL","Samedi"=>"Python","Dimanche"=>"Tout");
+var_dump($activites);
 
 
+
+asort($activites[Dimanche] = "repos du guerrier"); //change Sunday activity with the function "asort"
+var_dump($activites);
 //deux dimensions
 $pierre = array("1"=>"Php","2"=>"Javascript","3"=>"Python");
 $paul = array("1"=>"Javascript","2"=>"Css","3"=>"Php");
 $jacques = array("1"=>"Java","2"=>"Php","3"=>"Javascript");
 
-$preferences = array();
+$preferences = array_merge($pierre, $paul, $jacques); //join the 3 arrays with the function array_merge 
+var_dump($preferences);
 
+$preferences = array(
+    $pierre = array("1"=>"Php","2"=>"Javascript","3"=>"Python"),
+    $paul = array("1"=>"Javascript","2"=>"Css","3"=>"Php"),
+    $jacques = array("1"=>"Java","2"=>"Php","3"=>"Javascript"),
+
+); 
+var_dump($preferences);
 
 ?>
